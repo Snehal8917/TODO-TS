71550d8e7de203b5a58d6f6c6818962a361749db
snehal <snehalprajapati8917@gmail.com>
2024-08-04 23:41:40 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import {
+  Container,
+  TextField,
+  AppBar,
+  Stack,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import TodoItem from "./components/TodoItem";
+import { getTodos, saveTodos } from "./utils/features";
+
+const App = () => {
+  const [todos, setTodos] = useState<TodoItemType[]>(getTodos());
+
+  const [title, setTitle] = useState<TodoItemType["title"]>("");
+
+  const completeHandler = (id: TodoItemType["id"]): void => {
+    const newTodos: TodoItemType[] = todos.map((i) => {
+      if (i.id === id) i.isCompleted = !i.isCompleted;
+      return i;
+    });
+    setTodos(newTodos);
+  };
+
+  const deleteHandler = (id: TodoItemType["id"]): void => {
+    const newTodos: TodoItemType[] = todos.filter((i) => i.id !== id);
+    setTodos(newTodos);
+  };
+
+  const editHandler = (
+    id: TodoItemType["id"],
+    newTitle: TodoItemType["title"]
+  ): void => {
+    console.log("Edit handler called with new title:", newTitle);
+    const newTodos: TodoItemType[] = todos.map((i) => {
+      if (i.id === id) i.title = newTitle;
+      return i;
+    });
+    setTodos(newTodos);
+  };
+
+  const SubmitHandler = (): void => {
+    const newTodo: TodoItemType = {
+      title,
+      isCompleted: false,
+      id: String(Math.random() * 1000),
+    };
+    setTodos((prev) => [...prev, newTodo]);
+    setTitle("");
+  };
+
+  useEffect(() => {
+    saveTodos(todos);
+  }, [todos]);
+
+  const isAddButtonDisabled = title === "";
+
+  return (
+    <Container maxWidth="sm" sx={{ height: "100vh" }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Todo App</Typography>
+        </Toolbar>
+      </AppBar>
+      <Stack height={"80%"} direction={"column"} spacing={"1rem"} p={"1rem"}>
+        {todos.map((i) => (
+          <TodoItem
+            completeHandler={completeHandler}
+            deleteHandler={deleteHandler}
+            key={i.id}
+            todo={i}
+            editHandler={editHandler}
+          />
+        ))}
+      </Stack>
+      <TextField
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+        label={"Add Task"}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && title !== "") SubmitHandler();
+        }}
+      ></TextField>
+      <Button
+        sx={{ margin: "1rem 0" }}
+        fullWidth
+        variant="contained"
+        onClick={SubmitHandler}
+        disabled={isAddButtonDisabled}
+      >
+        Add
+      </Button>
+    </Container>
+  );
+};
+
+export default App;
